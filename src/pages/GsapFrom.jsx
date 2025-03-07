6ea2653c07acf1775e8ffad067449478bf78d1fb
Ssiyam0123 <ssiyam563@gmail.com>
2025-03-07 22:16:00 +0600

    gsap from

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+
+  useGSAP(()=>{
+    gsap.to('#green-box',{
+      x: 250,
+      repeat:-1,
+      yoyo:true,
+      rotate:360,
+      ease: 'elastic'
+    })
+  },[])
+
 
   return (
     <main>
